207091d8089f29e01459cb0c9ae761ace0149251
Alejandro Sierra <alexsierra1598@gmail.com>
2022-06-27 22:47:38 -0400

    styling horizontal scroll on activities

--- src/components/base/CardList.tsx
+++ src/components/base/CardList.tsx
@@ -1,15 +1,16 @@
 import './CardListStyle.css';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import React from 'react';
 
 // create shared interface
 interface CardData {
-  id: string;
-  date?: Date; // completion date,
-  title: string;
-  description?: string;
   category?: string;
+  date?: Date; // completion date,
   daysUntil?: number;
+  description?: string;
+  id: string;
+  imgUrl?: string;
+  title: string;
 }
 
 interface Props {
@@ -19,7 +20,19 @@ interface Props {
 const CardList: React.FC<Props> = ({ data }) => (
   <div className="card-list-container">
     {data.map((d) => (
-      <Card key={d.id} sx={{ maxWidth: 275 }}>
+      <Card
+        key={d.id}
+        sx={{
+          minWidth: 250,
+        }}
+      >
+        <CardMedia
+          component="img"
+          src={d.imgUrl || ''}
+          height="150"
+          width="100%"
+          alt="Image describing media"
+        />
         <CardContent>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             {d.category}
@@ -27,6 +40,7 @@ const CardList: React.FC<Props> = ({ data }) => (
           <Typography variant="h5" component="div">
             {d.title}
           </Typography>
+
           <Typography variant="body2">{d.description}</Typography>
         </CardContent>
       </Card>
--- src/components/pages/displayPage/DisplayPage.tsx
+++ src/components/pages/displayPage/DisplayPage.tsx
@@ -9,12 +9,13 @@ import './DisplayPageStyle.css';
 
 // create shared interface
 interface CardData {
-  id: string;
-  date?: Date; // completion date,
-  title: string;
-  description?: string;
   category?: string;
+  date?: Date; // completion date,
   daysUntil?: number;
+  description?: string;
+  id: string;
+  imgUrl?: string;
+  title: string;
   timeUntil?: number; // this could be in miliseconds then converted to hours/ minutes
 }
 
@@ -25,6 +26,8 @@ const cardData: CardData[] = [
     description: 'Visit the pantheon with friends.',
     category: 'travel',
     daysUntil: 0,
+    imgUrl:
+      'https://images.unsplash.com/photo-1605256526285-c0980b8c7805?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
     timeUntil: 2,
   },
   {
@@ -33,7 +36,39 @@ const cardData: CardData[] = [
     description: 'Have to try a crepe a la maison!',
     category: 'dining',
     daysUntil: 0,
+    imgUrl:
+      'https://images.unsplash.com/photo-1584278860047-22db9ff82bed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
     timeUntil: 5,
+  },
+  {
+    id: '3',
+    title: 'Atelier des lumiers',
+    description: 'Van Gogh brought to life',
+    category: 'travel',
+    daysUntil: 0,
+    imgUrl:
+      'https://images.unsplash.com/photo-1622572402015-2f8cde493c83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=884&q=80',
+    timeUntil: 7,
+  },
+  {
+    id: '4',
+    title: 'Cafe Renard',
+    description: 'Sprited away plays in the background',
+    category: 'dining',
+    daysUntil: 0,
+    imgUrl:
+      'https://images.unsplash.com/photo-1521017432531-fbd92d768814?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+    timeUntil: 9,
+  },
+  {
+    id: '5',
+    title: 'Palace Versailles',
+    description: `C'est pas versailles ici`,
+    category: 'travel',
+    daysUntil: 0,
+    imgUrl:
+      'https://images.unsplash.com/photo-1591828353335-197466da2a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    timeUntil: 11,
   },
 ];
 
