497f09becee0217fd6d11897d137f27dfd2f326c
Alejandro Sierra <alexsierra1598@gmail.com>
2022-08-29 23:24:52 -0400

    Add controlled form

--- src/components/base/DestinationForm.tsx
+++ src/components/base/DestinationForm.tsx
@@ -1,5 +1,5 @@
 import { Button, TextField } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   formValues?: Object;
@@ -7,27 +7,55 @@ interface Props {
 }
 
 // eslint-disable-next-line no-unused-vars
-const DestinationForm: React.FC<Props> = ({ formValues, onSubmit }) => (
-  <div>
-    <form>
-      <TextField
-        id="city"
-        label="city"
-        variant="standard"
-        placeholder="Please enter city"
-      />
-      <TextField
-        id="dates"
-        label="Dates"
-        variant="standard"
-        placeholder="Please enter dates (need react datepicker compoenent)"
-      />
-      <Button type="submit" variant="contained">
-        Submit
-      </Button>
-    </form>
-  </div>
-);
+const DestinationForm: React.FC<Props> = ({ formValues, onSubmit }) => {
+  // @ts-ignore
+  const [formState, setFormState] = useState({
+    city: '',
+    arrivalDate: '',
+    departureDate: '',
+  });
+
+  // @ts-ignore
+  const handleChange = (e, key) => {
+    const val = e.target.value;
+    // @ts-ignore
+
+    setFormState((prevState) => ({ ...prevState, [key]: val }));
+  };
+  return (
+    <div>
+      <form>
+        <TextField
+          id="city"
+          label="city"
+          variant="standard"
+          value={formState.city}
+          onChange={(e) => handleChange(e, 'city')}
+          placeholder="Please enter city"
+        />
+        <TextField
+          id="dates-arrival"
+          label="Arrival Date"
+          variant="standard"
+          value={formState.arrivalDate}
+          onChange={(e) => handleChange(e, 'arrivalDate')}
+          placeholder="Please enter arrival date (need react datepicker compoenent)"
+        />
+        <TextField
+          id="dates-departure"
+          label="Departure Date"
+          variant="standard"
+          value={formState.departureDate}
+          onChange={(e) => handleChange(e, 'departureDate')}
+          placeholder="Please enter departure date (need react datepicker compoenent)"
+        />
+        <Button variant="contained" onClick={() => console.log(formState)}>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 DestinationForm.defaultProps = {
   formValues: {},
